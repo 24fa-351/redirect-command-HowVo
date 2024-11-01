1dc0c31c4a7dfb870396b78f03bf52ff95ffe540
Hao Doan Anh Vo <146135155+HowVo@users.noreply.github.com>
2024-10-31 18:30:22 -0700

    Finished!

--- redir.c
+++ redir.c
@@ -3,9 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-char *findAbsolutePath(char *cmd)
+void parseCmd(char *cmd, char **args)
+{ // Parse the command into an array of arguments
+    char *token = strtok(cmd, " ");
+    int i = 0;
+    while (token != NULL)
+    {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+}
+
+char *findAbsolutePath(char *cmd) // Find the absolute path of the command
 {
+    if (cmd[0] == '/')
+        return strdup(cmd);
     char *path = getenv("PATH");
     if (!path)
         return NULL;
@@ -23,6 +38,8 @@ char *findAbsolutePath(char *cmd)
             return strdup(fullPath); // Return the first found path
         }
     }
+    free(pathCopy);
+    return NULL;
 }
 
 int main(int argc, char *argv[])
@@ -34,17 +51,18 @@ int main(int argc, char *argv[])
     }
 
     char *inp = argv[1];
+    char *cmd = argv[2];
     char *out = argv[3];
 
-    char **newargv = (char **)malloc(sizeof(char *) * (1 + argc - 2));
-    for (int i = 2; i < argc; i++)
+    char **args = (char **)malloc(sizeof(char *) * (1 + argc - 2));
+    parseCmd(cmd, args);
+    char *commandPath = findAbsolutePath(args[0]);
+
+    if (!commandPath)
     {
-        newargv[i - 2] = argv[i];
+        fprintf(stderr, "Command not found %s\n", args[0]);
+        exit(EXIT_FAILURE);
     }
-    newargv[argc - 2] = NULL;
-    char *commandPath = findAbsolutePath(newargv[0]);
-    printf("Absolute path %s\n", commandPath);
-    printf("newargv %s %s %s\n", newargv[0], newargv[1], newargv[2]);
 
     pid_t childPid = fork();
 
@@ -54,54 +72,24 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    else if (childPid == 0) // in child process
+    else if (childPid == 0) // In child process
     {
-        int fdIn, fdOut;
-
-        if (strcmp(inp, '-') != 0)
+        if (strcmp(inp, "-") != 0)
         {
-            fdIn = open(inp, O_RDONLY, 0644);
-
-            if (fdIn == -1)
-            {
-                perror("Can't open input file");
-                exit(EXIT_FAILURE);
-            }
-
-            if (dup2(fdIn, STDIN_FILENO) == -1)
-            {
-                perror("Can't redirect stdin");
-                exit(EXIT_FAILURE);
-            }
-            close(fdIn);
+            int fdin = open(inp, O_RDONLY, 0644);
+            dup2(fdin, STDIN_FILENO);
+            close(fdin);
         }
 
         if (strcmp(out, "-") != 0)
         {
-            fdOut = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (fdOut == -1)
-            {
-                perror("Error opening output file");
-                exit(EXIT_FAILURE);
-            }
-            if (dup2(fdOut, STDOUT_FILENO) == -1)
-            {
-                perror("Error redirecting stdout");
-                exit(EXIT_FAILURE);
-            }
-            close(fdOut);
+            int fdout = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            dup2(fdout, STDOUT_FILENO);
+            close(fdout);
         }
-
-        char *commandPath = findAbsolutePath(newargv[0]);
-
-        if (!commandPath)
-        {
-            fprintf(stderr, "Command not found: %s\n", newargv[0]);
-            exit(EXIT_FAILURE);
-        }
-        execve(commandPath, newargv, NULL);
-
-        wait(NULL);
-        return 0;
+        execve(commandPath, args, NULL);
     }
+
+    wait(NULL);
+    return 0;
 }
